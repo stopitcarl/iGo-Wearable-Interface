d83d066d3ea028d9026fa932f86e933201221c16
stopitcarl <joaofporto@tecnico.ulisboa.pt>
2019-04-01 16:20:45 +0100

    Lab work. Fixed stuff broke stuff. sick of this ****

--- iGo/projeto/main.js
+++ iGo/projeto/main.js
@@ -1,10 +1,8 @@
 function updateTime() {
     var now = new Date();
     document.getElementById("time").innerHTML = now.toLocaleTimeString().substring(0, 5);
-}
-function updateDate() {
-    var now = new Date();
     document.getElementById("date").innerHTML = now.toLocaleDateString();
+
 }
-setInterval(updateTime, 1000);
-setInterval(updateDate, 1000);
+
+setInterval(updateDate, 60000);
