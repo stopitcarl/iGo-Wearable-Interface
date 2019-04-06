e10930a2a7f0a6af030349e85a14ed04c20b91c1
stopitcarl <joaofporto@tecnico.ulisboa.pt>
2019-04-06 18:13:31 +0100

    fuck neves and fuck coutinho.. they contributed nothing to the project. Reminder: do solo projects

--- iGo/main.js
+++ iGo/main.js
@@ -5,24 +5,80 @@ function updateTime() {
 
 }
 
-var last_known_scroll_position = 0;
-var ticking = false;
-
-function doSomething(scroll_pos) {
-    console.log("Scrolling in fucntion");
+var Positions = {
+    top: [-0.4, -0.4],
+    center: [1.7, 1.7],
+    bottom: [3, 0.3],
+    out: [-2, 2],
 }
 
-var toggle = false;
+// Model options in objects (because fuck me, that's why)
+class Options {
+    constructor(id, pos, width, isRound, link) {
+        this.id = id;
+        this.position = pos;
+        this.width = width;
+        this.isRound = isRound;
+        this.link = "translater.html";
 
-function scrolla() {
-    if (!toggle)
-        $('#option-1').addClass("anim1");
-    else {
-        $('#option-1').addClass("anim2");
-        //        $('#option-1').removeClass("anim1");
+        $("#" + id).click(function () {
+            alert("opening " + this.link);
+        });
     }
 
-    toggle = toggle ? false : true;
+    rotate() {
+        var x = 0
+        var y = 0
+        switch (this.position) {
+            case Positions.top:
+                x = Positions.center[0] - this.position[0];
+                y = Positions.center[1] - this.position[1];
+                this.position = Positions.center;
+                $("#" + this.id).css("width", "2.8cm");
+                break;
+            case Positions.center:
+                x = Positions.bottom[0] - this.position[0];
+                y = Positions.bottom[1] - this.position[1];
+                this.position = Positions.bottom;
+                $("#" + this.id).css("width", "1.5cm");
+                break;
+            case Positions.bottom:
+                x = Positions.out[0] - this.position[0];
+                y = Positions.out[1] - this.position[1];
+                this.position = Positions.out;
+                break;
+            case Positions.out:
+                x = Positions.top[0] - this.position[0];
+                y = Positions.top[1] - this.position[1];
+                this.position = Positions.top;
+                break;
+            default:
+                break;
+        }
+        console.log(this.id, "translating", x, "and", y);
+        var attr = "translate(" + x + "cm," + y + "cm)";
+        $("#" + this.id).css("transform", attr);
+
+
+
+    }
+}
+
+var options = [];
+
+function init() {
+    options.push(new Options("option-1", Positions.top, 1.5, true, "translator.html"))
+    options.push(new Options("option-2", Positions.center, 2.8, false, "translator.html"))
+    options.push(new Options("option-3", Positions.bottom, 2.8, true, "translator.html"))
+    options.push(new Options("option-4", Positions.out, 2.8, true, "translator.html"))
+}
+
+var scroll = 0;
+
+function scrolla() {
+    options.forEach(o => {
+        o.rotate();
+    });
 }
 
 window.addEventListener('scroll', function (e) {
