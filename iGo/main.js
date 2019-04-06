9dda266b7f4df5c43fe0b95132c7af43c2fee9a0
João Porto <joaofporto@tecnico.ulisboa.pt>
2019-04-06 13:19:48 +0100

    Added animation framework

--- iGo/main.js
+++ iGo/main.js
@@ -5,6 +5,38 @@ function updateTime() {
 
 }
 
+var last_known_scroll_position = 0;
+var ticking = false;
+
+function doSomething(scroll_pos) {
+    console.log("Scrolling in fucntion");
+}
+
+var toggle = false;
+
+function scrolla() {
+    if (!toggle)
+        $('#option-1').addClass("anim1");
+    else {
+        $('#option-1').addClass("anim2");
+        //        $('#option-1').removeClass("anim1");
+    }
+
+    toggle = toggle ? false : true;
+}
+
+window.addEventListener('scroll', function (e) {
+    console.log("listener activated");
+    last_known_scroll_position = window.scrollY;
+    if (!ticking) {
+        window.requestAnimationFrame(function () {
+            doSomething(last_known_scroll_position);
+            ticking = false;
+        });
+    }
+    ticking = true;
+});
+
 function rotateDown() {
     $('.wrapper').toggleClass('tiny');
 }
