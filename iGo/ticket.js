3d4c6ba377b79937387638d76a457912cde416f8
João Porto <joaofporto@tecnico.ulisboa.pt>
2019-04-28 15:48:22 +0100

    Added some more feature because why not add features. feature are great but like we could less features though, just saying. But hey it's features so who cares.. Not brisson, he likes features except for the letter A, he doesn't like the A from Arial. It's too cmplex he says. Well brisson, just you watch how complex things can get. Im gonnna create a font with the simplest letter, my people will look at it and will say "wow that is a simple font" but the letter A. oh boy, it's gonna be complex af. Peace

--- iGo/ticket.js
+++ iGo/ticket.js
@@ -5,41 +5,53 @@ var activeTickets = [];
 function loadTickets() {
     console.log("lamo");
     $(document).ready(function () {
-        console.log("creating tickets");
         $.ajax({
             type: 'GET',
             url: 'assets/tickets.json',
             success: function (data) {
-                console.log("creating tickets");
                 tickets = data;
-
                 for (let i = 0; i < 5; i++) // Add only 2 tickets (naughty)
-                    createTicketHtml(tickets[i]);
+                    createTicket(tickets[i]);
             }
         });
     });
+
+
+
+
+    var myFunction = function (i) {
+        console.log(i);
+    }
+    let filters = document.getElementsByClassName("filter-icon");
+    let i = 0;
+    Array.from(filters).forEach(function (element) {
+        element.addEventListener('click', myFunction, i++);
+    });
+
 }
 
 function createTicketHtml(ticket) {
-    activeTickets.push(ticket);
+    fav = ticket.isFav ? '<td><img src="images/filters/star.png" class="filter-icon small"></td>' : '';
 
-    console.log("creating tickets");
+    console.log("creating ticket");
     let id = activeTickets.length;
     let ticketHTML = '<div id="ticket-' + id + '" class="polaroid">\
                         <img src="' + ticket.img + '" class="polaroid-image">\
                             <table class="ticket-table">\
                                 <tr>\
                                     <td class="ticket-name">' + ticket.name + '</td>\
-                                    <td class="button-ticket-information">\
-                                        <img src="images/information.png" class="ticket-information-image" onclick="checkInfo(' + id + ')">\
-                                    </td>\
-                                </tr>\
-                            </table>\
-                    </div>';
+                                    ' + fav + '<td class = "button-ticket-information">' +
+        '<img src = "images/information.png" class="ticket-information-image" \
+                                    onclick = "checkInfo(' + id + ')" >  </td>   </tr></table></div>';
+
     $('#ticket-container-main').append(ticketHTML);
 }
 
-
+function createTicket(ticket) {
+    activeTickets.push(ticket);
+    localStorage.setItem("tickets", JSON.stringify(activeTickets));
+    createTicketHtml(ticket);
+}
 
 var isOpen = 0;
 
@@ -77,24 +89,38 @@ function changeTicketsScreen(curScreen, targetScreen) {
     }
 
     $('#' + targetScreen).hide().fadeIn("slow", function () {
-        // Animation complete.
-        console.log("got in");
+        // Animation complete.        
     });
+
     $('#' + curScreen).fadeOut("slow", function () {
         // Animation complete.
-
-        console.log("got out");
     });
+
     if (targetScreen === "tickets-scan") {
+        // Insert the right icon
+        setTimeout(function () {
+            $("#found-ticket").fadeIn("fast"),
+                function () {}
+        }, 1000);
+
+        // Change screen
         setTimeout(changeTicketsScreen, 2000, "tickets-scan", "tickets-main");
+        // Remove the right icon
+        setTimeout(function () {
+            $("#found-ticket").fadeOut("fast"),
+                function () {}
+        }, 2000);
     }
 }
 
+var active_ticket = null;
+
 function checkInfo(ticketId) {
     // Fill modal with correct info    
-    ticket = tickets[ticketId - 1];
-    $('#ticket-title').html(ticket.name);
-    $('#ticket-description').html(ticket.info);
+    active_ticket = tickets[ticketId - 1];
+    $('#ticket-title').html(active_ticket.name);
+    $('#ticket-description').html(active_ticket.info);
+    $('#ticket-isFav').attr("src", active_ticket.isFav ? "images/filters/star.png" : "images/filters/star-empty.png");
 
     $('#ticket-info-modal').modal();
     $('.modal-backdrop').appendTo('.ticket-container');
@@ -104,11 +130,15 @@ function checkInfo(ticketId) {
 }
 
 function favorite(ticket) {
-
-    if (ticket.src.indexOf("images/star-empty.png") != -1) {
+    if (!active_ticket.isFav) {
         ticket.src = "images/filters/star.png"
+        active_ticket.isFav = true;
     } else {
-        ticket.src = "images/star-empty.png";
+        ticket.src = "images/filters/star-empty.png";
+        active_ticket.isFav = false;
     }
-
+    $('#ticket-container-main').html(""); // Clean ticket list
+    activeTickets.forEach(ticket => {
+        createTicketHtml(ticket);
+    });
 }
