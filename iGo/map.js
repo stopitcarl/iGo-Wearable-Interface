cea6a262cd2678f721f8967647e1ba0f5db3f3a7
BolachasDaAvo <miguelscfneves@gmail.com>
2019-05-05 10:00:29 +0100

    zoom = gooooooood

--- iGo/map.js
+++ iGo/map.js
@@ -1,13 +1,9 @@
 $(document).ready(function () {
-
-    $("#map").draggable();
-
     var places = JSON.parse(localStorage.getItem("places"));
     $.ajax({
         type: 'GET',
         url: 'assets/map.json',
         success: function (data) {
-            tickets = data;
             if (places == null || places.length < data.length) {
                 console.log("criar de novo");
                 places = [];
@@ -16,21 +12,4 @@ $(document).ready(function () {
             }
         }
     });
-});
-
-function zoom(zoomincrement) {
-    img_ele = document.getElementById('map');
-    var pre_width = img_ele.getBoundingClientRect().width,
-        pre_height = img_ele.getBoundingClientRect().height;
-    img_ele.style.width = (pre_width * zoomincrement) + 'px';
-    img_ele.style.height = (pre_height * zoomincrement) + 'px';
-    img_ele = null;
-}
-
-$(window).bind('mousewheel', function (e) {
-    if (e.originalEvent.wheelDelta > 0) {
-        zoom(1.1);
-    } else {            
-        zoom(0.9);
-    }
 });
